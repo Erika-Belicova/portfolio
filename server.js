41975a6877addd33b0d9b2f6c737b12411970afd
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-07-11 16:08:51 +0200

    Fix server static path to serve Angular build from dist/portfolio/browser

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/portfolio')));
+app.use(express.static(path.join(__dirname, 'dist/portfolio/browser')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/portfolio/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/portfolio/browser/index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
